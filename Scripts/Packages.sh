72057b500e0139e77bd8c02122ffca7567c249ba
TJQ <61875111+tjq19940331@users.noreply.github.com>
2025-12-22 21:49:47 +0800

    Add new packages to Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -50,13 +50,15 @@ UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
-
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "rtp2httpd" "stackia/rtp2httpd" "main"
+UPDATE_PACKAGE "adguardhome" "sirpdboy/luci-app-adguardhome" "main"
+UPDATE_PACKAGE "lucky" "sirpdboy/luci-app-lucky" "main"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
